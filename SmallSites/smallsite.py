acf0b344d20afbae9a549800aeb6022fd5cd5939
David Jaffe <david.ezekial.jaffe@gmail.com>
2020-09-02 16:53:13 -0400

    first complete analysis of ggus tickets

--- SmallSites/smallsite.py
+++ SmallSites/smallsite.py
@@ -7,7 +7,7 @@ parse GGUS_tickets.csv from Cedric
 import sys,os
 import csv
 import operator
-
+import xlrd
 
 #import datetime
 
@@ -20,6 +20,10 @@ class smallsite():
         self.debug = 0
 
         self.csvfn = 'GGUS_tickets.csv'
+
+        self.resource_est = '/Users/djaffe/Documents/Belle II/Software_Computing/ResourceEstimates/20200616/ResourceEstimate-2020-06-16.xlsx'
+
+        self.gridsites = 'GridSites_20200902.xlsx'
 
         self.noRDC = True
         self.invalidSite = ['']
@@ -79,20 +83,140 @@ return dict[site] = [ subject1, subject2, ...]
             if Subj.lower()==s : return False
 
         return True
-    def tixBySite(self,Tix):
+    def tixBySite(self,Tix,show=False):
         '''
-        produce table of number of tickets by site
+        produce table of number of tickets by site and by country
+        return dict[country] = ntickets
         '''
         ntix = {}
+        ntixC= {}
         for site in Tix:
             ntix[site] = len(Tix[site])
         sorted_sites = sorted(ntix.items(), key=operator.itemgetter(1),reverse=True)
+        if show : print '{0:>10} {1:>30} {2:>20}'.format('#tickets','Site','Country')
         for x in sorted_sites:
-            print x[1],x[0]
-        return
-    def main(self):
+            Site = x[0]
+            country = self.assignCtoS(Site)
+            if show : print '{0:>10} {1:>30} {2:>10}'.format(x[1],Site,country)
+            if country not in ntixC: ntixC[country] = 0
+            ntixC[country] += x[1]
+        if show : print '{0:>10} {1:>20}'.format('#tickets','Country')
+        for country in ntixC:
+            if show : print '{0:>10} {1:>20}'.format(ntixC[country],country)
+        return ntixC
+    def assignCtoS(self,Site):
+        '''
+        try to assign a country to each site
+        '''
+        country = None
+        gridS = self.gridS
+        for c in gridS:
+            for s in gridS[c]:
+                if Site==s or Site in s or Site.split('-')[0] in s:  # exact match or Site contained in name
+                    country = c
+                    break
+        
+        if country is None:
+            print 'smallsite.assignCtoS Try special treatment for Site',Site
+            if 'INFN' in Site : country = 'Italy'
+            if 'TW-'  in Site : country = 'Taiwan'
+                
+        return country
+    def gridSites(self):
+        '''
+        return dict[country] = [site1,site2...]
+        '''
+        wb = xlrd.open_workbook(self.gridsites)
+        sheet = wb.sheet_by_name('Sheet1')
+        colC  = 0 # country name
+        colS  = 1 # Site
+        colGN = 2 # grid name
+        gridS = {}
+        for row in range(340):
+            country = sheet.cell_value(row,colC)
+            if country!='Country' and country!='':
+                if country not in gridS: gridS[country] = []
+                gn = sheet.cell_value(row,colGN)
+                site = sheet.cell_value(row,colS)
+                if self.debug>0 : print 'smallsite.gridSites country,site,gridname',country,site,gn
+                gridS[country].append(gn)
+        return gridS
+    def getRE(self,Year=2021,show=False):
+        '''
+        return dict[country] = PB of disk in Year
+
+        Navigate to proper rows for countries and column for Year
+        '''
+        wb = xlrd.open_workbook(self.resource_est)
+        sheet = wb.sheet_by_name('Total Cross-check')
+
+        # find rows delimiting range of cells with country names
+        # and the row with the year
+        col = 0
+        diskRow = None
+        totalRow= None
+        for row in range(100):
+            cell = sheet.cell_value(row,col)
+            if self.debug>1 : print 'smallsite.getRE row,col,cell',row,col,cell
+            if cell=='Disk (PB)':
+                if self.debug>0 : print 'smallsite.getRE Found row',row,cell
+                diskRow = row
+            if diskRow is not None and 'Total' in cell:
+                if self.debug>0 : print 'smallsite.getRE Found row',row,cell
+                totalRow = row
+                break
+
+        # find the column for the requested year
+        yearRow = diskRow - 2
+        yearCol = None
+        for col in range(15):
+            cell = sheet.cell_value(yearRow,col)
+            if cell==Year:
+                yearCol = col
+                break
+        cell = sheet.cell_value(yearRow,yearCol)
+        if self.debug>0 : print 'smallsite.getRE yearRow,yearCol,year',yearRow,yearCol,cell
+        # get country names and pledged disk(PB)
+        RE = {}
+        col = 0
+        sumDisk = 0.
+        for row in range(diskRow+1,totalRow):
+            country = sheet.cell_value(row,col)
+            disk    = sheet.cell_value(row,yearCol)
+            RE[country] = disk
+            sumDisk += disk
+        if show : 
+            print '{0:>8} {1:>8} {2} Year is {3}'.format('Disk(PB)','Fraction','Country',Year)
+            sorted_RE = sorted(RE.items(), key=operator.itemgetter(1))
+            for x in sorted_RE:
+                country = x[0]
+                disk = x[1]
+                print '{0:>8.3f} {1:>8.3f} {2}'.format(disk,disk/sumDisk,country)
+        return RE
+    def justify(self,ntixC,RE,Year):
+        '''
+        Table of tickets/country and fraction disk/country
+        '''
+        sorted_RE = sorted(RE.items(), key=operator.itemgetter(1))
+        sumDisk = 0.
+        for country in RE:
+            sumDisk += RE[country]
+        
+        print '{0:>8} {1:>8} {2:>8} {3}. Uses {4} resource estimate.'.format('Disk(PB)','Fraction','#tickets','Country',Year)
+        for x in sorted_RE:
+            country = str(x[0])
+            disk = x[1]
+            n = 0
+            if country in ntixC: n = ntixC[country]
+            print '{0:>8.3f} {1:>8.3f} {2:>8} {3}'.format(disk, disk/sumDisk,n,country)
+        return            
+    def main(self,Year=2021):
+        self.gridS = self.gridSites()
         Tix = self.rdr()
-        self.tixBySite(Tix)
+        ntixC = self.tixBySite(Tix) # return number of tickets per country
+        RE = self.getRE(Year=Year)   # resources per country
+        self.justify(ntixC, RE,Year)
+
         return
 if __name__ == '__main__' :
 
