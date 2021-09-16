87f522a2959fc270f4b3b5412239e2f82dc12d93
David Jaffe <david.ezekial.jaffe@gmail.com>
2021-09-16 15:18:32 -0400

    add plot2 - calculations of gain, effy

--- Plotter/plotter.py
+++ Plotter/plotter.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 class plotter():
     def __init__(self):
         self.params=[7.89,1.086,2.255,4333.]
+        self.maxADC = 20000.
         return
     def iTOPspe(self,x):
         '''
@@ -26,7 +27,7 @@ class plotter():
         '''
         compare function used for spe with gaussian
         '''
-        x = numpy.linspace(0,20000.)
+        x = numpy.linspace(1.,self.maxADC,num=int(self.maxADC))
         
         y = self.iTOPspe(x)
         plt.plot(x,y,'-')
@@ -43,6 +44,39 @@ class plotter():
         plt.grid()
         plt.show()
         return
+    def plot2(self):
+        '''
+        claim by Kojima 20191022 b2gm_top_monitor_muroyama.pdf
+        is Gain = integral( x*f(x) dx ) / integral( f(x) dx )
+        where f(x) = iTOPspe function, x = ADC charge
+        Actually the eqn above just defines the <x> = expectation value of x. 
+
+        also estimate threshold in ADC charge from Kojima's 20201015 p15 where he defines efficiency = integral_threshold( f(x) dx ) / integral( f(x) dx ) and shows an efficiency of 0.996. 
+
+        '''
+        x = numpy.linspace(0.,self.maxADC,num=int(self.maxADC))
+        y = self.iTOPspe(x)
+        up = numpy.sum(x*y)
+        dn = numpy.sum(y)
+        Gain = -1.
+        if dn>0. : Gain = up/dn
+        print 'plotter.plot2 up,dn,Gain=',up,dn,Gain
+        #
+        # threshold estimate
+        effgoal = 0.996
+        for i,u in enumerate(x):
+            q = numpy.sum(y[i:])
+            eff = q/dn
+            if eff < effgoal:
+                effm1 = numpy.sum(y[i-1:])
+                m = (eff-effm1)/(u-x[i-1])
+                b = (effm1*u - eff*x[i-1])/(u-x[i-1])
+                thres = (effgoal - b)/m
+                print 'plotter.plot2 eff[x],x',eff,u,'effgoal,thres=',effgoal,thres
+                break
+            
+        
+        return
 if __name__ == '__main__':
     plot = plotter()
-    plot.plot1()
+    plot.plot2()
