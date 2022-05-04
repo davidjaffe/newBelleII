2235f1dda987c60fa107c443feb8fce3bc629520
David Jaffe <david.ezekial.jaffe@gmail.com>
2022-05-04 11:18:02 -0400

    Angelos suggestions to include LHCb msmts for Lc and Oc

--- Three/LcLife/charmlife.py
+++ Three/LcLife/charmlife.py
@@ -17,16 +17,30 @@ class charmlife():
         # PDG2020+2021 update
         self.life  = [ 1040., 410.1, 202.4, 268.]
         self.dlife = [    7.,   1.5,   3.1,  26.]
+
+        # LHCb Lambda c lifetime measurement
+        self.lambdacExpt = ['LHCb']
+        self.lambdacLife = [203.5]
+        self.lambdacDLife= [math.sqrt(1.0*1.0+1.3*1.3+1.4*1.4)]
+        
         # previous Omega c lifetime measurements
+        # LHCb results
+        # prompt Omegac SciBull67,479(2021)
+        # Omegac PRL121,092003 (2018) [add 0.001 fs to make value != PDG value for plotting manipulations]
+        # Lambdac PRD100,032001 (2019)
         # uncertainties are symmetrized by taking average of up,down unc and summing stat+syst in quadrature
-        self.omegacExpt = ['FOCUS', 'WA89', 'E687']
-        self.omegacLife = [     72.,    55.,     86.]
-        self.omegacDLife= [math.sqrt(11.*11.+11.*11.),
+        self.omegacExpt = [ 'LHCb', 'LHCb(2021)', 'FOCUS', 'WA89', 'E687']
+        self.omegacLife = [ 268.001,  276.5,     72.,    55.,     86.]
+        self.omegacDLife= [
+                          math.sqrt(24.*24+10.*10.+2.*2),
+                          math.sqrt(13.4*13.4+4.4*4.4+0.7*0.7),
+                          math.sqrt(11.*11.+11.*11.),
                           math.sqrt(12.*12.+20.5*20.5),
-                          math.sqrt(23.5*23.5+28.*28)]
-            # D+,D0 PRL 127, 211801 (2021).
-            # Jake's PRL draft v3-4
-            # Nisar's prez to Charm WG 20220427 BLINDED
+                          math.sqrt(23.5*23.5+28.*28)
+                               ]
+        # D+,D0 PRL 127, 211801 (2021).
+        # Jake's PRL draft v3-4
+        # Nisar's prez to Charm WG 20220427 BLINDED
         self.B2name  = ['Belle II\n'+x for x in ['published','published','prelim','blinded']]
         
         self.B2life  = [1030.4, 410.5, 203.20, 299.6]
@@ -77,12 +91,29 @@ class charmlife():
             dY.append(dtau)
             Gname.append(self.B2name[i])
 
+        print('\nLambda_c lifetimes')
+        x = x0 + 2*xstep - dx
+        for i,tau in enumerate(self.lambdacLife):
+            name,dtau = self.lambdacExpt[i],self.lambdacDLife[i]
+            print('{} {:.1f}({:.1f}) fs'.format(name,tau,dtau))
+            X.append(x)
+            x -= dx
+            Y.append(tau)
+            dY.append(dtau)
+            Gname.append(name)
 
+        AllValues = False
+        if AllValues:
+            print('\nAll values')
+            for i,y in enumerate(Y):
+                x,dy,name = X[i],dY[i],Gname[i]
+                print('{} {:.1f} {:.2f}+-{:.2f} fs'.format(name,x,y,dy))
+            
         NY = 4
         if NY==2: 
             fig, ax = plt.subplots(2,NY)#1,4)
         else:
-            fig, ax = plt.subplots(1,4)
+            fig, ax = plt.subplots(1,4, gridspec_kw={'width_ratios':[1,1,1.2,1.6]})
 
         TIGHT = False
         if not TIGHT: 
@@ -102,8 +133,10 @@ class charmlife():
             title = Title[J]
             
             U,V,dV,Name = [],[],[],[]
+            print('r',r)
             for i,name in enumerate(Gname):
                 x,y,dy = X[i],Y[i],dY[i]
+                #print('x',x,'y',y,'r[0]<=y<=r[1]',r[0]<=y<=r[1],'y in Used',y in Used)
                 if r[0]<=y<=r[1] and y not in Used:
                     Used.append(y)
                     U.append(x)
@@ -113,6 +146,9 @@ class charmlife():
             print('r',r,'len(U)',len(U),'U',U,'V',V,Name)
             
             a.errorbar(U,V,yerr=dV,label=Name,linestyle='none',marker='o')
+            if 'PDG' in Name:
+                j = Name.index('PDG')
+                a.errorbar(U[j],V[j],yerr=dV[j],linestyle='none',marker='o',color='black')
             A[0].set_ylabel('Lifetime (fs)',fontsize=15)
             
             for i,name in enumerate(Name):
@@ -128,7 +164,9 @@ class charmlife():
                 a.text(x,y,name,rotation=90,verticalalignment=va,horizontalalignment=ha,fontsize=12)
                 #print('x,y,name',x,y,name)
 
-            a.set_title(title,fontsize=15)
+            fs = 15
+            if title== r'$\Omega_c$' : fs = 14
+            a.set_title(title,fontsize=fs)
             a.tick_params(axis='x',which='both', bottom=False,top=False,labelbottom=False)          # changes apply to the x-axis, both major and minor ticks are affected,  ticks along the bottom edge are off,  ticks along the top edge are off
             
 
@@ -140,10 +178,10 @@ class charmlife():
         A[1].set_xlim((x0+xstep-hx,x0+xstep+1.5*dx))
         A[1].set_ylim(yrange[1])
 
-        A[2].set_xlim((x0+2*xstep-hx,x0+2*xstep+1.5*dx))
+        A[2].set_xlim((x0+2*xstep-1.5*dx,x0+2*xstep+1.5*dx))
         A[2].set_ylim(yrange[2])
 
-        A[3].set_xlim((x0+3*xstep-4*dx,x0+3*xstep+2*dx))
+        A[3].set_xlim((x0+3*xstep-6*dx,x0+3*xstep+2*dx))
         A[3].set_ylim(yrange[3])
 
         if TIGHT: plt.tight_layout()
