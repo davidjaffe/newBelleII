c8b5a5cb1fa03cf3fec0b266981e61c2393157fc
David Jaffe <david.ezekial.jaffe@gmail.com>
2022-09-02 09:48:08 -0400

    calculated expected reviews/institution

--- Three/Authors/authorStats.py
+++ Three/Authors/authorStats.py
@@ -9,8 +9,8 @@ import sys,os
 
 
 
-import matplotlib.pyplot as plt
-
+#import matplotlib.pyplot as plt
+import numpy
 
 
 class authorStats():
@@ -52,7 +52,7 @@ class authorStats():
             allRevInst.extend( pubInfo[pubNum] )
         totalInst = len(allRevInst)
         totalPubs = len(pubInfo)
-        uniqInst = set(allRevInst)
+        uniqInst = [a for a in set(allRevInst)]
         totalUniqInst = len(uniqInst)
         instFreq = {} # instFreq[inst] = freq
         freqShow = {} # freqShow[freq] = list of insts with that freq
@@ -75,8 +75,55 @@ class authorStats():
             if freq in freqShow: print(freq,','.join(sorted(freqShow[freq])))
 
         print(totalPubs,'total pubs reviewed by',totalInst,'institutions or  {:.2f} insts/pub. There are {} unique institution.'.format(totalInst/totalPubs,totalUniqInst))
+
+        self.expected(totalPubs,uniqInst,totalInst/totalPubs)
                 
         return
+    def fillFreqShow(self,uniqInst,LIST):
+        '''
+        fill freqShow = dict = freqShow[freq] = list of institutions with frequency freq
+        '''
+        freqShow = {}
+        for inst in uniqInst:
+            freq = LIST.count(inst)
+            if freq not in freqShow : freqShow[freq] = []
+            freqShow[freq].append(inst)
+        return freqShow
+    def expected(self,nPubs,uniqInst,instPerPub):
+        '''
+        calculate expected frequency distribution assuming nPubs total publications 
+        with reviewed by instPerPub instititons assigned to each publication.
+        Draw institutions from uniqInst list of institutions
+        '''
+        nExpt = 1000
+        grandFreq = {}
+        for freq in range(nPubs+1):
+            grandFreq[freq] = 0
+        for i in range(nExpt):
+            whole = []
+            for p in range(nPubs):
+                r = self.select(uniqInst, int(instPerPub) )
+                whole.extend(r)
+            freqShow = self.fillFreqShow(uniqInst,whole)
+
+            for freq in freqShow:
+                nold = grandFreq[freq]
+                n = len(freqShow[freq])
+                grandFreq[freq] = nold + n
+
+        print('authorStats.expected For',nExpt,'expts, frequency/inst assuming',int(instPerPub),'institutions must review each pub')
+        tot = 0
+        for freq in grandFreq: tot += grandFreq[freq]
+        print('{:>8} {:>8} {:>12}'.format('Freq','Number','Probability'))
+        for freq in grandFreq:
+            if grandFreq[freq]>0: print('{:>8} {:>8} {:>12.4f}'.format(freq,grandFreq[freq],(grandFreq[freq])/float(tot)))
+        return
+    def select(self,list,n):
+        '''
+        return n unique items from list without replacement
+        '''
+        if self.debug>2: print('authorStats.select n',n,'list',list)
+        return numpy.random.choice(list,n,replace=False)
     def main(self):
         pubInfo = self.reader(self.dataFile)
         self.processStats(pubInfo)
